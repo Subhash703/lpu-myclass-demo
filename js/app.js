ee81dcc63c5be2749a6df759e79e252ba8fd40ce
Subhash Chandra <subhashchandra30720@gmail.com>
2020-09-26 10:39:21 +0530

    Notification API added

--- js/app.js
+++ js/app.js
@@ -1,53 +1,69 @@
 const username = document.getElementById('username');
-        const password = document.getElementById('password');
-        const captcha = document.getElementById('captcha');
-        const captchaText = document.querySelector('.captacha-box');
-        const loginBtn = document.querySelector('.login__btn');
-        const changeBtn = document.querySelector('.change-btn');
+const password = document.getElementById('password');
+const captcha = document.getElementById('captcha');
+const captchaText = document.querySelector('.captacha-box');
+const loginBtn = document.querySelector('.login__btn');
+const changeBtn = document.querySelector('.change-btn');
 
-        const backgrounds = ['red','blue','cyan','green','yellow'];
+const backgrounds = ['red','blue','cyan','green','yellow'];
 
-        changeBtn.addEventListener('click',()=>{
-            // captcha.
-            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            let newCaptcha = '';
-            for(let i = 0; i<6; i++){
-                newCaptcha += characters.charAt(Math.floor(Math.random()*characters.length));
-            }
-            captchaText.style.backgroundColor=backgrounds[Math.floor(Math.random()*backgrounds.length)];
-            captchaText.innerHTML = newCaptcha;
-        });
+captchaText.innerText = captchaText.style.backgroundColor=backgrounds[Math.floor(Math.random()*backgrounds.length)];
 
-        loginBtn.addEventListener('click',(e)=>{
-            e.preventDefault();
-            let timeid;
-            if(username.value===""){
-                timeid = setInterval(timerFunction(username),500);
-            }
-            
-            else if(password.value===""){
-                timeid = setInterval(timerFunction(password), 500);
-            }
-            
-            else if(captcha.value===""){
-                timeid = setInterval(timerFunction(captcha), 500);
-            }
+window.onload = ()=>{
+    let promise = Notification.requestPermission();
+    console.log(promise);
+}
 
-            else if(captcha.value!=captchaText.innerText){
-                // console.log(captcha.value,captchaText.innerText);
-                timeid = setInterval(timerFunction(captcha), 500);
-            }
-            
-            else{
-                myStorage = window.localStorage;
-                myStorage.setItem('Username',username.value);
-                myStorage.setItem('Password',password.value);
-                window.location.href="./landing.html";
-            }
-            clearInterval(timeid);
-        });
+changeBtn.addEventListener('click',()=>{
+    // captcha.
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let newCaptcha = '';
+    for(let i = 0; i<6; i++){
+        newCaptcha += characters.charAt(Math.floor(Math.random()*characters.length));
+    }
+    captchaText.style.backgroundColor=backgrounds[Math.floor(Math.random()*backgrounds.length)];
+    captchaText.innerHTML = newCaptcha;
+});
 
-        function timerFunction(element){
-            alert(element.getAttribute('name')+" Is empty")
-            element.focus();
-        }
+loginBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    let timeid;
+    if(username.value===""){
+        timeid = setInterval(timerFunction(username),500);
+    }
+    
+    else if(password.value===""){
+        timeid = setInterval(timerFunction(password), 500);
+    }
+    
+    else if(captcha.value===""){
+        timeid = setInterval(timerFunction(captcha), 500);
+    }
+
+    else if(captcha.value!=captchaText.innerText){
+        // console.log(captcha.value,captchaText.innerText);
+        timeid = setInterval(timerFunction(captcha), 500);
+    }
+    
+    else{
+        myStorage = window.localStorage;
+        myStorage.setItem('Username',username.value);
+        myStorage.setItem('Password',password.value);
+        window.location.href="./landing.html";
+    }
+    clearInterval(timeid);
+});
+
+function timerFunction(element){
+    alert(element.getAttribute('name')+" Is empty")
+    element.focus();
+}
+
+let count = 0
+while(count<13){
+    let id = setInterval(fun(),3000);
+
+    clearInterval(id);
+    count+=3;
+}
